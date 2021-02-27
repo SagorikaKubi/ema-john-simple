dbcf90e524891b5575faa8fadad6602672064731
SagorikaKubi <sagorika.tech@gmail.com>
2021-02-27 20:44:16 +0600

    added

---
+++ src/components/Product/Product.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css';
+
+const Product = (props) => {
+    console.log(props);
+    const { name, img, seller, price, stock } = props.product;
+    return (
+        <div className="product" >
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div className="product-div">
+                <h4 className="product-name">{name}</h4>
+                <br />
+                <p><small>By: {seller}</small></p>
+                <p>${price}</p>
+                <br />
+                <p><small>Only {stock} left in stock -Order soon</small></p>
+                <button className="main-card" onClick={() => props.handleAddProduct(props.product)}><FontAwesomeIcon icon={faShoppingCart} />
+                 Add to cart</button>
+            </div>
+        </div>
+    );
+
+
+};
+
+export default Product;
